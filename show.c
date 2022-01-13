b810f7097d21319648f384b5c6f4b0106b9d570f
NaiSyuan <jeffrey1234567891047@gmail.com>
2022-01-13 18:45:44 +0800

    show

--- show.c
+++ show.c
@@ -14,6 +14,10 @@ int remain[13] = { 4 };
 int ans;
 const char* flower[4] = { "Hearts","Diamonds","Clubs","Spades" };
 
+
+void show(player x);
+void showwhowin(player x, player z);
+
 void show(player x)
 {
 	int i;
@@ -53,4 +57,49 @@ void show(player x)
 
 		printf("玩家2的總和 : %.1f \n", you2.total);
 	}
+}
+
+
+void showwhowin(player x, player z)
+{
+	if ((x.total = you.total) && (z.total = computer.total))
+	{
+		if ((x.total > z.total) && (x.total <= 10.5) && (z.total <= 10.5))
+		{
+			printf("\n=====玩家1 Win=====\n");
+		}
+		else if ((z.total > x.total) && (z.total <= 10.5) && (x.total <= 10.5))
+		{
+			printf("\n*****Computer Win*****\n");
+		}
+		else if ((z.total == x.total) && (z.total <= 10.5) && (x.total <= 10.5))
+		{
+			printf("\n-----Tie-----\n");
+		}
+		else
+		{
+			printf("XXXXX No One Win XXXXX\n");
+		}
+	}
+
+	else if ((x.total = you.total) && (z.total = you2.total))
+	{
+		if ((x.total > z.total) && (x.total <= 10.5) && (z.total <= 10.5))
+		{
+			printf("\n=====玩家1 Win=====\n");
+		}
+		else if ((z.total > x.total) && (z.total <= 10.5) && (x.total <= 10.5))
+		{
+			printf("\n*****玩家2 Win*****\n");
+		}
+		else if ((z.total == x.total) && (z.total <= 10.5) && (x.total <= 10.5))
+		{
+			printf("\n-----Tie-----\n");
+		}
+		else
+		{
+			printf("XXXXX No One Win XXXXX\n");
+		}
+	}
+
 }
