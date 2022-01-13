0b1a3f4ca489d6aa848f1f4f4fabbf9b8ea32ba2
a3b4 <chunkai97692512@gmail.com>
2022-01-13 18:51:07 +0800

    main

--- source/main.c
+++ source/main.c
@@ -1,10 +1,109 @@
+int main()
+{
 
-showwhowin(you, computer);
+	int who, i;
 
-printf("\n");
+	computer.total = 0;
+	you.total = 0;
+	you2.total = 0;
 
-again();
-break;
+	computer.name = 'c';
+	you.name = 'y';
+	you2.name = 'q';
+
+
+	for (i = 0; i < 6; i++)
+	{
+		computer.tims[i] = 0;
+		you.tims[i] = 0;
+		you2.tims[i] = 0;
+	}
+
+	printf("開始遊戲 要與誰玩\n");
+	printf("1 與電腦玩 2兩個人玩 0退出遊戲\n");
+	scanf_s("%d", &who);
+	srand(time(NULL));
+
+	do
+	{
+		switch (who)
+		{
+		case 1:
+			i = 0;
+			getcard(computer, i);
+			getcard(you, i);
+
+			printf("D = 10\n");
+			printf("電腦的牌 : X  \n");
+			printf("你的牌 :   %c \n", face[you.tims[0] - 1]);
+
+			addcard();
+
+			i = 1;
+			while (ans == 1)
+			{
+				getcard(you, i);
+
+				printf("\n你的牌 : %c \n", face[you.tims[i] - 1]);
+				i++;
+
+				if (you.total <= 10.5 && you.tims[4] != 0)
+				{
+					show(you);
+					printf("你過五關\n=====You Win=====\n");
+					again();
+				}
+				if (you.total > 10.5)
+				{
+					show(you);
+					printf("\n*****Computer Win*****\n");
+					again();
+				}
+
+				addcard();
+			}
+
+
+			printf("輪到電腦\n");
+
+
+			if (computer.total < 10)
+			{
+				i = 1;
+
+				while (computer.total < 8)
+				{
+					getcard(computer, i);
+
+					printf("\n電腦的牌 : %c \n", face[computer.tims[i] - 1]);
+					i++;
+
+					if (computer.total <= 10.5 && computer.tims[4] != 0)
+					{
+						show(computer);
+						printf("電腦過五關 \n*****Computer Win*****\n");
+						again();
+					}
+					if (computer.total > 10.5)
+					{
+						show(computer);
+						printf("\n=====You Win=====\n");
+						again();
+					}
+				}
+			}
+
+			printf("\n攤牌\n\n");
+
+			show(computer);
+			show(you);
+
+			showwhowin(you, computer);
+
+			printf("\n");
+
+			again();
+			break;
 
 		case 2:
 			i = 0;
